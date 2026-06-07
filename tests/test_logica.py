9bde52a3997b4273cf81955eff7254f1db6e1dc1
MateusAndrade-1809 <mateusandrademotta@gmail.com>
2026-06-07 12:09:17 -0300

    test: cobrir regras basicas do alvo

--- tests/test_logica.py
+++ tests/test_logica.py
@@ -1,4 +1,10 @@
-from src.funcoes import calcular_pontos, jogador_perdeu, limitar_valor
+from src.funcoes import (
+    calcular_pontos,
+    clique_acertou_alvo,
+    jogador_perdeu,
+    limitar_valor,
+    tomar_dano,
+)
 
 
 def test_calcular_pontos():
@@ -16,6 +22,11 @@ def test_jogador_nao_perdeu_com_vidas():
     assert jogador_perdeu(3) is False
 
 
+def test_tomar_dano():
+    """Deve reduzir a quantidade de vidas."""
+    assert tomar_dano(3, 1) == 2
+
+
 def test_limitar_valor_abaixo_do_minimo():
     """Deve retornar o limite minimo quando o valor informado for menor."""
     assert limitar_valor(-5, 0, 100) == 0
@@ -29,3 +40,18 @@ def test_limitar_valor_acima_do_maximo():
 def test_limitar_valor_dentro_do_intervalo():
     """Deve manter o valor original quando ele ja estiver no intervalo."""
     assert limitar_valor(50, 0, 100) == 50
+
+
+def test_clique_acertou_alvo_no_centro():
+    """Deve acertar quando o clique ocorre no centro do alvo."""
+    assert clique_acertou_alvo((100, 100), (100, 100), 35) is True
+
+
+def test_clique_acertou_alvo_na_borda():
+    """Deve acertar quando o clique ocorre exatamente na borda."""
+    assert clique_acertou_alvo((135, 100), (100, 100), 35) is True
+
+
+def test_clique_errou_alvo():
+    """Deve errar quando o clique ocorre fora do raio."""
+    assert clique_acertou_alvo((140, 100), (100, 100), 35) is False
